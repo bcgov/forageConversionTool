9d2d37ad8165d8d4dad173fdf74679e6a1c92473
micheal-w-wells <micheal.w.wells@gmail.com>
2019-08-06 15:33:34 -0700

    add code to make forage data table, needs rest of columns

--- forageConversionTool.py
+++ forageConversionTool.py
@@ -4,4 +4,7 @@ def connect_to_database(databaseName):
 	conn = sqlite3.connect(databaseName) # or use :memory: to put it in RAM
 	cursor = conn.cursor()
 	return cursor
-	
+
+def create_table(cursor):
+	sql_statement = """create table forage_data (site_id varchar, species_name varchar) """
+	cursor.execute(sql_statement)
--- tests.py
+++ tests.py
@@ -9,7 +9,14 @@ def test_can_connect_to_db():
 		assert False
 
 def test_can_create_table():
-	assert False
+	# write some code that creates a table in forageConversionTool and then 
+	aCursor = forageConversionTool.connect_to_database('nancydb')
+	forageConversionTool.create_table(aCursor)
+
+	# write some code here that checks for it using this sql:
+	aCursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='forage_data';")
+	rows = aCursor.fetchall()
+	assert len(rows) == 1
 
 def test_can_load_data():
 	assert False
